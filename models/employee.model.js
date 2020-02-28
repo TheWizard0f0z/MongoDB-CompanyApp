df8dddce04cda73ccea557e8bc7f2239f7c4c92c
Marcin Dzieszkowski <m.dzieszkowski@gmail.com>
2020-02-28 21:31:46 +0100

    add populating the data inside the reference function

--- models/employee.model.js
+++ models/employee.model.js
@@ -4,7 +4,7 @@ const employeeSchema = new mongoose.Schema(
   {
     firstName: { type: String, required: true },
     lastName: { type: String, required: true },
-    department: { type: String, required: true }
+    department: { type: String, required: true, ref: 'Department' }
   },
   {
     versionKey: false
